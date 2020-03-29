ee4e802794d116d02fbf98b576191c41cb591b11
nehakrishnakumar <nehakk.2014@gmail.com>
2020-03-28 21:29:23 -0400

    Fixed syntax errors and hard-coded (instead of using environment) auth keys

--- send_sms.js
+++ send_sms.js
@@ -1,13 +1,12 @@
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-
-const client = require('twilio')(accountSid, authToken);
+const accountSid='ACaebfb3e4ad8b2040735d948452538e7e';
+const authToken='c005fa545056781b692556e0684cd65b';
+const client=require('twilio')(accountSid, authToken);
 
 client.messages
 	.create ({
 		body: 'kill this love bam bam bam bam bam bam yeahyeah yeah yeah!',
-		from: '+15713785918',
-		to: '+17038287144'
+		to: '+17038287144',
+		from: '+15713785918'
 	})
 	.then(message => console.log(message.sid));
